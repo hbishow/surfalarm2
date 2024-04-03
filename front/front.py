01ec66defca0b6f25d11627e1a436d95d4c353fe
Henry Bichot <82338099+hbishow@users.noreply.github.com>
2024-04-03 21:40:31 +0200

    Add files via upload

---
+++ front/front.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import pandas as pd
+import boto3
+from io import StringIO
+from geopy.geocoders import Nominatim
+
+def read_csv_from_s3(bucket_name, file_key):
+    """Read a CSV file from S3 into a Pandas DataFrame."""
+    
+    session = boto3.Session(
+        aws_access_key_id = 'AKIAUWDEBVLESY4ZTDNC',
+        aws_secret_access_key ='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',
+        region_name='eu-west-3'
+        )
+    
+    # Initialize an S3 client with boto3
+    s3_client = session.client('s3')
+    
+    # Get the object from the bucket
+    csv_obj = s3_client.get_object(Bucket=bucket_name, Key=file_key)
+    csv_string = csv_obj['Body'].read().decode('utf-8')
+    
+    # Use Pandas to read the CSV data
+    df = pd.read_csv(StringIO(csv_string))
+    
+    return df
+
+
+def get_file(bucket_name):
+    ###This function gets the most  recent file
+    
+    session = boto3.Session(
+    aws_access_key_id = 'AKIAUWDEBVLESY4ZTDNC',
+    aws_secret_access_key ='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',
+    region_name='eu-west-3'
+    )
+
+    # Initialize the S3 client
+    s3 = session.client('s3')
+    # List objects in the bucket
+    response = s3.list_objects_v2(Bucket=bucket_name)
+
+    # Sort objects by last modified timestamp
+    objects = sorted(response['Contents'], key=lambda obj: obj['LastModified'], reverse=True)
+
+    # Get the key (file name) of the most recent object
+    most_recent_file = objects[0]['Key']
+
+    return most_recent_file
+
+# Example usage in your Streamlit app
+bucket_name = "surf-spot-conditions"
+file_key = "2023-06-09.csv"
+#file_key = get_file(bucket_name)
+
+# Read the CSV file
+df = read_csv_from_s3(bucket_name, file_key)
+
+# Display the DataFrame in your app
+st.write(df)
+
